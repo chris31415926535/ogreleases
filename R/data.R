85926a5c185f1d94352203aedb79096633df6379
Christopher Belanger <christopher.a.belanger@gmail.com>
2020-09-17 12:12:57 -0400

    Initial commit.

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' Selection of Government of Ontario news releases from 2019-2020
+#'
+#' @format A data frame with 1503 rows and 7 variables.
+#'
+#' @source \url{https://news.ontario.ca/en/}
+"ogreleases_data"
